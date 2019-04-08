8471f8d65312ef210d0ceb4f779d40bcfc8743f1
Marshilla Brahma <marshilla.brahma@gmail.com>
2019-04-08 09:48:19 -0400

    Completed object orientation section

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,21 +1,25 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+    private String value;
     public PalindromeObject(String input) {
+        value = input;
     }
 
     public String[] getAllPalindromes(){
-        return null;
+        return PalindromeEvaluator.getAllPalindromes(value);
     }
 
     public Boolean isPalindrome(){
-        return null;
+        return PalindromeEvaluator.isPalindrome(value);
     }
 
     public String reverseString(){
-        return null;
+        return PalindromeEvaluator.reverseString(value);
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringEvaluatorObject.java
@@ -1,21 +1,25 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.StringEvaluator;
+
 /**
  * @author leon on 19/12/2018.
  */
 public class StringEvaluatorObject {
+    private String value;
     public StringEvaluatorObject(String str) {
+        value = str;
     }
 
     public String[] getAllSubstrings() {
-        return null;
+        return StringEvaluator.getAllSubstrings(value);
     }
 
     public String[] getCommonSubstrings(String secondInput) {
-        return null;
+        return StringEvaluator.getCommonSubstrings(value,secondInput);
     }
 
     public String getLargestCommonSubstring(String secondInput) {
-        return null;
+        return StringEvaluator.getLargestCommonSubstring(value,secondInput);
     }
 }
